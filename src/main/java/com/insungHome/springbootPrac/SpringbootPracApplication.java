19e34fc1183376e4b740479da8538ae5bd62e041
SUNGIN99 <99_insung@naver.com>
2022-04-14 19:08:24 +0900

    어렵다 spring..

--- src/main/java/com/insungHome/springbootPrac/SpringbootPracApplication.java
+++ src/main/java/com/insungHome/springbootPrac/SpringbootPracApplication.java
@@ -3,6 +3,10 @@ package com.insungHome.springbootPrac;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//main 클래스
+// 프로젝트 가장 상단에 위치
+
+// template에 html 파일이 들어갈거임(view파일)
 @SpringBootApplication
 public class SpringbootPracApplication {
 
