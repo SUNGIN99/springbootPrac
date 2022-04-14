bebe083ba3dcbce650258e4324f6380154453211
SUNGIN99 <99_insung@naver.com>
2022-04-14 18:28:27 +0900

    Initial commit 22_04_14) 스프링 설치

---
+++ src/test/java/com/insungHome/springbootPrac/SpringbootPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insungHome.springbootPrac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
